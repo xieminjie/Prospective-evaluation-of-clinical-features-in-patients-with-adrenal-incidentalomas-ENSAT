03242fb891812219b0a3a51ec0454c6f71288cf6
xieminjie <xieminjiexmj@gmail.com>
2016-03-11 11:38:14 +1100

    updated

--- ClientApp/app/src/main/java/com/example/xieminjie/clientapp/MainActivity.java
+++ ClientApp/app/src/main/java/com/example/xieminjie/clientapp/MainActivity.java
@@ -11,26 +11,26 @@ import android.widget.TextView;
 import io.socket.client.Socket;
 
 public class MainActivity extends AppCompatActivity {
-    private Socket socket;
-    private String userID;
     private EditText loginTextField;
     private Button loginBtn;
+    private String userID;
     public static final String TAG="myActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initInterface();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userID = loginTextField.getText().toString();
+                startToLogin();
+            }
+        });
     }
     private void initInterface(){
         loginTextField = (EditText)findViewById(R.id.main_loginTextField);
         loginBtn = (Button)findViewById(R.id.main_loginBtn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startToLogin();
-            }
-        });
     }
     private void startToLogin(){
         Intent intent = new Intent(this, Register.class);
