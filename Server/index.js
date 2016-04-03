7abd92f7a49fedc5789860b60a6b005bdc713170
xieminjie <xieminjiexmj@gmail.com>
2016-04-04 09:19:06 +1000

    route update

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,17 @@
 
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 var mysql = require('mysql');
+
+app.use(express.static('views'));
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/views/html/index.html');
+});
 
 var connection = mysql.createConnection({
   host: 'localhost',
