af0a69b195063f7d239b90f6b03ec005c0776938
xieminjie <xieminjiexmj@gmail.com>
2016-04-04 10:02:15 +1000

    update

--- Server/views/js/index.js
+++ Server/views/js/index.js
@@ -1,4 +1,31 @@
 
 $(document).ready(function(){
 	var socket = io();
+	renderChart();
 });
+
+function renderChart(){
+	$('#container').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+}
