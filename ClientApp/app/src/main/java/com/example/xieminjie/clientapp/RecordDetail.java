217219bdabc591233d1450a593fac253d2679a1e
xieminjie <xieminjiexmj@gmail.com>
2016-04-12 23:37:45 +1000

    update

--- ClientApp/app/src/main/java/com/example/xieminjie/clientapp/RecordDetail.java
+++ ClientApp/app/src/main/java/com/example/xieminjie/clientapp/RecordDetail.java
@@ -16,6 +16,7 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
@@ -24,8 +25,6 @@ public class RecordDetail extends AppCompatActivity implements SearchView.OnQuer
     private SearchView searchView;
     private String query;
     private ArrayList<Record> arrayList;
-    private ArrayList<String> dateArrayList;
-    private ArrayList<String> dataArrayList;
     private IOStorageHandler ioStorageHandler;
     private Hashtable hashtable;
     private Record record;
@@ -45,29 +44,27 @@ public class RecordDetail extends AppCompatActivity implements SearchView.OnQuer
         Intent intent = getIntent();
         query = intent.getStringExtra("searchName");
         index = getIndex(query);
-        createChart(index,barChart,query);
+        createChart(index, barChart, query);
 
     }
     private int getIndex(String query){
-        arrayList = getDate(ioStorageHandler.readRecordLog("record.csv", getApplicationContext()));
-
         Hashtable<String,String> hashtable = new Hashtable();
         hashtable.put("problem","0");
         hashtable.put("ill","1");
         hashtable.put("palpitations","2");
         hashtable.put("weightGain","3");
         hashtable.put("highBloodPressure","4");
-        hashtable.put("muscleWeakness","5");
-        hashtable.put("sweating","6");
-        hashtable.put("flushing","7");
-        hashtable.put("headache","8");
-        hashtable.put("chestPain","9");
-        hashtable.put("backPain","10");
-        hashtable.put("bruising","11");
-        hashtable.put("fatigue","12");
-        hashtable.put("panic","13");
+        hashtable.put("muscleWeakness", "5");
+        hashtable.put("sweating", "6");
+        hashtable.put("flushing", "7");
+        hashtable.put("headache", "8");
+        hashtable.put("chestPain", "9");
+        hashtable.put("backPain", "10");
+        hashtable.put("bruising", "11");
+        hashtable.put("fatigue", "12");
+        hashtable.put("panic", "13");
         hashtable.put("sadness","14");
-        hashtable.put("recordDate","15");
+        hashtable.put("recordDate", "15");
         if (hashtable.containsKey(query)){
             index = Integer.parseInt(hashtable.get(query).toString());
         }else{
@@ -76,55 +73,53 @@ public class RecordDetail extends AppCompatActivity implements SearchView.OnQuer
         return index;
     }
     private void createChart(int index,BarChart barChart,String query){
+        ArrayList<String> dateArrayList;
+        ArrayList<String> dataArrayList;
+        ArrayList<String> dateArray;
+        ArrayList<String> dataArray;
+
         dateArrayList = ioStorageHandler.readData("record.csv", 15, getApplicationContext());
-        dataArrayList = ioStorageHandler.readData("record.csv",index,getApplicationContext());
-        BarData data = new BarData(getXAxisValues(dateArrayList),getDataSet(dataArrayList,query));
+        dataArrayList = ioStorageHandler.readData("record.csv", index, getApplicationContext());
+        dateArray = dateProcessing(dateArrayList);
+        dataArray = dateProcessing(dataArrayList);
+        BarData data = new BarData(getXAxisValues(dateArray),getDataSet(dataArray,query));
         barChart.setData(data);
         barChart.setDescription("My Chart");
         barChart.animateXY(2000, 2000);
         barChart.invalidate();
 
     }
-    private ArrayList getDate(ArrayList<Record> recordArrayList){
-        ArrayList dateArrayList = new ArrayList();
-        int day = recordArrayList.size();
-        if(day<=5){
-            for(int i=recordArrayList.size()-1;i>=0;i--){
-                dateArrayList.add(recordArrayList.get(i).getRecord_date().toString());
+    private ArrayList<String> dateProcessing(ArrayList a){
+        ArrayList<String> array = new ArrayList<>();
+        if(a.size()<=5){
+            for(int i=0;i<5;i++){
+                array.add(a.get(i).toString());
             }
         }else{
-            for(int i=recordArrayList.size()-1;i>=recordArrayList.size()-5;i--){
-                dateArrayList.add(recordArrayList.get(i).getRecord_date().toString());
+            for(int i = a.size()-1;i>=a.size()-5;i--){
+                array.add(a.get(i).toString());
             }
         }
-        return dateArrayList;
+        return array;
     }
-
-    private ArrayList<IBarDataSet> getDataSet(ArrayList dataArrayList,String query) {
+    private ArrayList<IBarDataSet> getDataSet(ArrayList dataArray,String query) {
         ArrayList<IBarDataSet> dataSets;
-        ArrayList<BarEntry> valueSet = new ArrayList<>();
-        for(int i =dataArrayList.size()-1;i>=0;i--){
-            BarEntry barEntry = new BarEntry(Integer.parseInt(dataArrayList.get(i).toString()),dataArrayList.size()-1-i);
-            valueSet.add(barEntry);
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        for (int i=0;i<dataArray.size();i++){
+            BarEntry v = new BarEntry(Integer.valueOf(dataArray.get(i).toString()),i);
+            valueSet1.add(v);
         }
-        BarDataSet barDataSet = new BarDataSet(valueSet,query);
-        barDataSet.setColor(Color.rgb(0,155,0));
+        BarDataSet barDataSet = new BarDataSet(valueSet1,query);
+        barDataSet.setColor(Color.rgb(0, 155, 0));
         dataSets = new ArrayList<>();
         dataSets.add(barDataSet);
         return dataSets;
     }
 
-
-    private ArrayList<String> getXAxisValues(ArrayList dateArrayList) {
+    private ArrayList<String> getXAxisValues(ArrayList dateArray) {
         ArrayList<String> xAxis = new ArrayList<>();
-        if (dateArrayList.size()<=5){
-            for(int i=0;i<dateArrayList.size();i++){
-                xAxis.add(dateArrayList.get(i).toString());
-            }
-        }else{
-            for(int i=dateArrayList.size()-6;i<dateArrayList.size();i++){
-                xAxis.add(dateArrayList.get(i).toString());
-            }
+        for(int i=0;i<dateArray.size();i++){
+            xAxis.add(dateArray.get(i).toString());
         }
         return xAxis;
     }
